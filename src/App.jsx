9818d219587f9ba82eb54da172385c27312d0a4c
Nina Persson <ninap21@hotmail.com>
2025-08-20 13:02:06 +0200

    första commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="root">
+        <p>Hej</p>
+      </div>
+    </>
+  )
+}
+
+export default App
