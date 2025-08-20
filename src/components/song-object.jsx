94fe0c5b8b4dc0b848e0a6ae5765569b98c4d980
Nina Persson <ninap21@hotmail.com>
2025-08-20 14:13:50 +0200

    deleted unnecessary file

--- src/components/song-object.jsx
+++
@@ -1,9 +0,0 @@
-class Song {
-    constructor(id, title, author, melody, text) {
-        this.id = id
-        this.title = title
-        this.author = author
-        this.melody = melody
-        this.text = text
-    }
-}
