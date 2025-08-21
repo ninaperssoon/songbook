f18dfa01b6847ad08eb0c7d7ce8f09451980b8ab
Nina Persson <ninap21@hotmail.com>
2025-08-21 15:34:24 +0200

    active class on nav

--- src/components/songs.jsx
+++ src/components/songs.jsx
@@ -7,14 +7,20 @@ function Songs() {
     const [songs, setSongs] = useState([])
     const [allSongs, setAllSongs] = useState([])
 
+    
+
     useEffect(() => {
-      async function fetchSongs() {
-        const data = await getSongData()
-        setSongs(data)
-        setAllSongs(data)
-      }
-  
-      fetchSongs()
+        async function fetchSongs() {
+            const data = await getSongData()
+            setSongs(data)
+            setAllSongs(data)
+            
+        }
+        fetchSongs()
+
+        const all = document.getElementById('All')
+        all.classList.add('active')
+
     }, [])
 
     const filterSongs = (e) => {
@@ -34,6 +40,23 @@ function Songs() {
 
     function filterByCategory(category) {
 
+        const element = document.getElementById(category)
+        element.classList.add('active')
+
+        const all = document.getElementById('All')
+        const vajan = document.getElementById('Vajan')
+        const norrlands = document.getElementById('Norrlands')
+        const classics = document.getElementById('Klassiker')
+        const friends = document.getElementById('Vänner')
+        
+        const categories = [all, vajan, norrlands, classics, friends]
+
+        for (const item of categories) {
+            if (item !== element) {
+                item.classList.remove('active')
+            }
+        }
+
         if (category === 'All') {
             setSongs(allSongs)
             return
@@ -50,11 +73,11 @@ function Songs() {
     return (
         <div>
             <ul>
-                <li><a onClick={() => filterByCategory('All')}>Alla sånger</a></li>
-                <li><a onClick={() => filterByCategory('Vajan')}>Vajan</a></li>
-                <li><a onClick={() => filterByCategory('Norrlands')}>Norrlands</a></li>
-                <li><a onClick={() => filterByCategory('Klassiker')}>Klassiker</a></li>
-                <li><a onClick={() => filterByCategory('Vänner')}>Vänner</a></li>
+                <li><a id='All' onClick={() => filterByCategory('All')}>Alla sånger</a></li>
+                <li><a id='Vajan' onClick={() => filterByCategory('Vajan')}>Vajan</a></li>
+                <li><a id='Norrlands' onClick={() => filterByCategory('Norrlands')}>Norrlands</a></li>
+                <li><a id='Klassiker' onClick={() => filterByCategory('Klassiker')}>Klassiker</a></li>
+                <li><a id='Vänner' onClick={() => filterByCategory('Vänner')}>Vänner</a></li>
             </ul>
 
             <input type='text' id='search-songs' onChange={filterSongs} placeholder='Sök efter sång'></input>
